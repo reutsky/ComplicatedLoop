ad093ed52039fd3488da38f6f5acc02a07278776
Andrew <58339207+reutsky@users.noreply.github.com>
2020-05-05 17:50:45 -0400

    v2_Loop
    I wrote this during a lecture, the concept is a little bit different but the loop works now.

--- loop.c
+++ loop.c
@@ -5,8 +5,51 @@
 // I want the program to say 'User-loser.'
 
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-     printf("hello, world\n");
+    int i = 0;
+
+    // In case of a disagreement - push
+    while (i < 2)
+    {
+
+        // Prompt user for an answer
+        char answer = get_char("Do you agree?\n");
+
+        // Do we agree?
+        if (answer == 'y' || answer == 'Y')
+        {
+            printf("Agreed.\n");
+
+            // Shake hands and quit
+            i = i + 2;
+        }
+        else if (answer == 'n' || answer == 'N')
+        {
+
+            if (i == 0)
+            {
+                printf("Not agreed. I give you one more chance.\n");
+
+                // Push
+                i++;
+            }
+            else if (i == 1)
+            {
+                printf("Not agreed.\n");
+
+                // Stop negotiating
+                i++;
+            }
+            
+        }
+        else
+        {
+            printf("Fuck off/n");
+            i = i + 2;
+        }
+
+    }
 }
